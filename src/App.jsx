4472c3b7d7d99ad91ad7aa28e1801bcef39a8768
Juan Sierra <sierra.juan99@gmail.com>
2024-04-12 14:12:32 -0700

    feat: sidebar and topbar component
    The page has been divided into two distinct sections to accommodate the newly added sidebar and topbar components. Each component now occupies its designated area, providing clear delineation and enhancing the overall organization of the interface.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import QuarterlyDataGraph from "./QuarterlyDataGraph";
+import TopBar from "./TopBar";
+import { Sidebar } from "./Sidebar";
 import "./App.css";
 function App() {
+  const [sidebarIsMini, setSidebarIsMini] = useState(false);
+  const toggleSidebar = () => {
+    setSidebarIsMini(!sidebarIsMini);
+  };
   return (
     <div id="root">
-      <div className="graph-container">
+      <div
+        className={`sidebar-section ${
+          sidebarIsMini ? "hide-sidebar-section" : ""
+        }`}
+      >
+        <Sidebar
+          buttonFunction={toggleSidebar}
+          simplifiedSidebar={sidebarIsMini}
+        />
+      </div>
+      <div
+        className={`main-page-section ${
+          sidebarIsMini ? "extended-main-page-section" : ""
+        }`}
+      >
+        <TopBar />
         <QuarterlyDataGraph />
       </div>
     </div>
