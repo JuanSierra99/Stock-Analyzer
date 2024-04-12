4eea8a5cf3917f66f6c72f2e15d2433fc7ac6cda
Juan Sierra <sierra.juan99@gmail.com>
2024-04-12 16:25:53 -0700

    style: Update chart appearance and y-axis number formatting
    The charts have been enhanced with a cleaner look and a new color scheme, improving their visual appeal. Additionally, the y-axis number values are now formatted in a way that significantly reduces the space required for large numbers. Numbers are converted to decimal format, with 'B' for billion, 'M' for million, and 'K' for thousands. These updates greatly enhance the readability of the charts and improve the overall user experience.

--- src/QuarterlyDataGraph.jsx
+++ src/QuarterlyDataGraph.jsx
@@ -13,6 +13,7 @@ import {
   Legend,
 } from "chart.js";
 import { Line, Bar } from "react-chartjs-2";
+import "./QuarterlyDataGraph.css";
 
 // Chose to register, so that we have smaller bundle sizes
 ChartJS.register(
@@ -27,25 +28,6 @@ ChartJS.register(
   Legend
 );
 
-const options = {
-  scales: {
-    y: {
-      ticks: {
-        callback: (value, index, ticks) => {
-          if (value >= 1e9 || value <= -1e9) {
-            return "$" + value / 1e9 + "B";
-          } else if (value >= 1e6 || value <= -1e6) {
-            return "$" + value / 1e6 + "M";
-          } else if (value >= 1e3 || value <= -1e3) {
-            return "$" + value / 1e3 + "K";
-          }
-          return "$" + value;
-        },
-      },
-    },
-  },
-};
-
 const QuarterlyIncomeGraph = ({ type, value }) => {
   const [netIncomeHistory, setNetIncomeHistory] = useState([]);
   const [fiscalDateHistory, setFiscalDateHistory] = useState([]);
@@ -55,6 +37,32 @@ const QuarterlyIncomeGraph = ({ type, value }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [displayAsBarGraph, setDisplayAsBarGraph] = useState(false);
+  const quarterlyNetIncomeColor = "#2a9d8f";
+  const quarterlyRevenueColor = "#e9c46a";
+  const quarterlyShareholderColor = "#e76f51";
+  const options = {
+    elements: {
+      point: {
+        radius: 7,
+      },
+    },
+    scales: {
+      y: {
+        ticks: {
+          callback: (value, index, ticks) => {
+            if (value >= 1e9 || value <= -1e9) {
+              return "$" + value / 1e9 + "B";
+            } else if (value >= 1e6 || value <= -1e6) {
+              return "$" + value / 1e6 + "M";
+            } else if (value >= 1e3 || value <= -1e3) {
+              return "$" + value / 1e3 + "K";
+            }
+            return "$" + value;
+          },
+        },
+      },
+    },
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -81,13 +89,14 @@ const QuarterlyIncomeGraph = ({ type, value }) => {
   if (loading) return <p>Loading Data...</p>;
   if (error) return <p>Error: {error}</p>;
   return (
-    <>
+    <div className="graph-container">
       <button
+        className="toggle-graph-type-button"
         onClick={() => {
           setDisplayAsBarGraph(!displayAsBarGraph);
         }}
       >
-        {displayAsBarGraph ? "Line" : "Bar"}
+        {displayAsBarGraph ? "Bar Chart" : "Line Chart"}
       </button>
       {displayAsBarGraph ? (
         <Bar
@@ -99,17 +108,23 @@ const QuarterlyIncomeGraph = ({ type, value }) => {
               {
                 label: "Quarterly Net Income",
                 data: netIncomeHistory,
-                backgroundColor: "red",
+                backgroundColor: quarterlyNetIncomeColor,
+                borderColor: "transparent",
+                hoverBackgroundColor: "#264653",
               },
               {
                 label: "Quarterly Revenue",
                 data: revenueHistory,
-                backgroundColor: "grey",
+                backgroundColor: quarterlyRevenueColor,
+                borderColor: "transparent",
+                hoverBackgroundColor: "#264653",
               },
               {
                 label: "Quarterly Shareholder Equity",
                 data: totalShareholderEquityHistory,
-                backgroundColor: "blue",
+                backgroundColor: quarterlyShareholderColor,
+                borderColor: "transparent",
+                hoverBackgroundColor: "#264653",
               },
             ],
           }}
@@ -123,23 +138,35 @@ const QuarterlyIncomeGraph = ({ type, value }) => {
               {
                 label: "Quarterly Net Income",
                 data: netIncomeHistory,
-                borderColor: "red",
+                backgroundColor: quarterlyNetIncomeColor,
+                borderColor: quarterlyNetIncomeColor,
+                pointBackgroundColor: "transparent",
+                pointBorderColor: "transparent",
+                hoverBackgroundColor: "#264653",
               },
               {
                 label: "Quarterly Revenue",
                 data: revenueHistory,
-                borderColor: "grey",
+                backgroundColor: quarterlyRevenueColor,
+                borderColor: quarterlyRevenueColor,
+                pointBackgroundColor: "transparent",
+                pointBorderColor: "transparent",
+                hoverBackgroundColor: "#264653",
               },
               {
                 label: "Quarterly Shareholder Equity",
                 data: totalShareholderEquityHistory,
-                borderColor: "blue",
+                backgroundColor: quarterlyShareholderColor,
+                borderColor: quarterlyShareholderColor,
+                pointBackgroundColor: "transparent",
+                pointBorderColor: "transparent",
+                hoverBackgroundColor: "#264653",
               },
             ],
           }}
         />
       )}
-    </>
+    </div>
   );
 };
 
