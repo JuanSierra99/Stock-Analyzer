33c53004fcc02563c755407edd47e49a6b37a1b9
Juan Sierra <sierra.juan99@gmail.com>
2024-04-13 18:54:59 -0700

    feat: search input field functions
    The search input field in this component can now be used to search for specific tickers.

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import "./TopBar.css";
-const Topbar = () => {
+
+const Topbar = ({ searchTickerValue, setSearchTickerValue }) => {
+  const [searchInput, setSearchInput] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Fetch stock data based on the entered symbol
+      setSearchTickerValue(searchInput);
+    } catch (error) {
+      console.error("Error fetching stock data:", error);
+    }
+  };
   return (
     <div className="topbar-container">
       <h1 className={"topbar-title"}>Dashboard</h1>
       <div className="topbar-right">
-        <input
-          type="text"
-          placeholder="Search Companies"
-          className="search-stocks"
-        ></input>
+        <form onSubmit={handleSubmit} className="search-stock-form">
+          <button type="submit" className="search-button">
+            <img src="/Svgs/search.svg" style={{ width: "15px" }} />
+          </button>
+          <input
+            type="text"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            placeholder="TICKER"
+            className="search-stocks"
+          ></input>
+        </form>
+
         <a>
           <img
             src="Svgs/open-book.svg"
